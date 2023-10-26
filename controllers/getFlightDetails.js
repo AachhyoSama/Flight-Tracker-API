59b4cb0c08b3cc2d164983916e65f0df0641d3ad
Anish Gurung <gurunganish1430@gmail.com>
2023-10-26 08:40:08 -0700

    minor updates

--- controllers/getFlightDetails.js
+++ controllers/getFlightDetails.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import dotenv from "dotenv";
+import * as dotenv from "dotenv";
 
 dotenv.config();
 
@@ -7,12 +7,14 @@ const apiKey = process.env.API_KEY || "test";
 
 export const getFlightDetails = async (flightNumber) => {
     const flightURL = `http://api.aviationstack.com/v1/flights?access_key=${apiKey}&flight_number=${flightNumber}`;
+    const headers = {
+        "Content-Type": "application/json",
+    };
 
     const flights = await axios.get(flightURL, {
-        headers: {
-            "Content-Type": "application/json",
-        },
+        headers,
     });
+
     // Just returning the first flight response for now
     return flights.data.data[0];
 };
--- index.js
+++ index.js
@@ -7,17 +7,14 @@ const app = express();
 const port = 8080;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 app.use("/flightTrackerAPI/v1/", tracker);
 
-https
-    .createServer(
-        {
-            key: fs.readFileSync("./key.pem"),
-            cert: fs.readFileSync("./cert.pem"),
-        },
-        app
-    )
-    .listen(port, () => {
-        console.log(`UpStreamAPI app server is running at port ${port}`);
-    });
+const httpsOptions = {
+    key: fs.readFileSync("key.pem"),
+    cert: fs.readFileSync("cert.pem"),
+};
+
+https.createServer(httpsOptions, app).listen(port, () => {
+    console.log(`UpStreamAPI app server is running at port ${port}`);
+});
